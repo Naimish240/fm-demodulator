92f0e6a5eb3cd43c0b465c1c1b2a91aa49e076fd
naimish240 <naimish240@gmail.com>
2024-06-21 20:50:48 +0530

    implemented live demodulator

--- fm_radio.py
+++ fm_radio.py
@@ -1,1 +1,64 @@
-# Contains the cli script to record iq samples and save audio to a file
+import asyncio
+from rtlsdr import RtlSdr
+import pyaudio
+from scipy.signal import bilinear, lfilter
+import numpy as np
+import argparse
+from spectrum_scan import scan
+import sys
+
+async def streaming(center_freq):
+    sdr = RtlSdr()
+    fs = 250e3
+    sdr.set_sample_rate(fs)
+    sdr.set_center_freq(center_freq*1e6)
+    N_samples = 1024*6*10
+    sample_rate_audio = fs/6
+    
+    pya = pyaudio.PyAudio()
+    stream = pya.open(format=pyaudio.paInt16,
+                      channels=1,
+                      rate=int(sample_rate_audio),
+                      output=True)
+    
+
+    async for samples in sdr.stream(N_samples):
+        try:
+            x = np.diff(np.unwrap(np.angle(samples)))
+            bz, az = bilinear(1, [75e-6, 1], fs=fs)
+            x = lfilter(bz, az, x)
+            x = x[::6]
+            x /= np.max(np.abs(x))
+            x *= 32767
+            x = x.astype(np.int16)
+            stream.write(x.tobytes())
+
+        except Exception as e:
+            print(e)
+            await sdr.stop()
+            stream.stop_stream()
+            stream.close()
+            pya.terminate()
+            sdr.close()
+            return None
+
+    # done
+    sdr.close()
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--scan', action='store_true')
+    parser.add_argument('--station', type=float)
+    args = parser.parse_args()
+
+    if args.scan:
+        scan()
+        sys.exit()
+    
+    if not 88 < args.station < 110:
+        print("... Invalid station frequency! Must be between 88 and 110!")
+        sys.exit()
+
+    loop = asyncio.get_event_loop()
+    loop.run_until_complete(streaming(args.station))
--- spectrum_scan.py
+++ spectrum_scan.py
@@ -1,3 +1,72 @@
 # Spectrum scanner to identify radio stations
-
 from rtlsdr import RtlSdr
+import numpy as np
+import matplotlib.pyplot as plt
+from matplotlib.mlab import psd
+import tqdm
+import pandas as pd
+
+
+def scan():
+    # Configure RTL-SDR
+    sdr = RtlSdr()
+    sdr.sample_rate = 2.048e6  # 2.048 MHz
+    sdr.center_freq = 99e6     # 99 MHz (this will be adjusted in the loop)
+    sdr.gain = 'auto'
+
+    # Frequency range
+    start_freq = 88e6
+    end_freq = 110e6
+    step_freq = 1e6
+    NFFT = 4096
+
+    # Store results
+    frequencies = []
+    psd_values = []
+
+    print("... Starting scan from 88 MHz to 110 MHz")
+
+    for freq in tqdm.tqdm(np.arange(start_freq, end_freq, step_freq)):
+        sdr.center_freq = freq
+        samples = sdr.read_samples(256*1024)
+        
+        # Calculate PSD
+        psd, freqs = plt.psd(samples, NFFT=NFFT, Fs=sdr.sample_rate / 1e6, Fc=sdr.center_freq / 1e6)
+        frequencies.extend(freqs)
+        psd_values.extend(psd)
+
+    # Close the SDR
+    plt.clf()
+    sdr.close()
+
+    frequencies = np.array(frequencies)
+    psd_values = np.array(psd_values)
+    psd_values = np.clip(psd_values, None, 30)
+
+    data = np.dstack((frequencies, psd_values)).reshape(frequencies.shape[0], 2)
+    data = data[np.argsort(data[:, 0])] 
+
+    df = pd.DataFrame(data, columns=["frequency", "psd"])
+
+    final_freq = []
+    thres = 5
+
+    for i in range(880, 1100, 1):
+        low = i/10 - 0.05
+        high = i/10 + 0.05
+        max_psd = max(df[(df.frequency > low) & (df.frequency < high)].psd)
+        if max_psd > thres:
+            final_freq.append(i/10)
+
+    if len(final_freq) == 0:
+        print("... No radio stations found that are strong enough to be demodulated!")
+        return None
+
+    final_freq = ' '.join([str(i)+' MHz' for i in final_freq])
+
+    print("... Possible Radio Stations found at :")
+    print("   ", final_freq)
+
+
+if __name__ == '__main__':
+    scan()
